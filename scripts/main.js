11ccd9c5e8864f59cad6498204495497fef062e0
Stephan Kok <stephan.kok.345@gmail.com>
2025-03-24 12:30:41 +0100

    Get Database and put it in html

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,48 @@
-async function getData() {
-    const url = "https://fdnd.directus.app/items/women_in_tech";
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-      const json = await response.json();
-      console.log(json);
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
+
+const url = "https://fdnd.directus.app/items/women_in_tech";
+const list = document.querySelector('section');
+
+getEverybody();
+
+
+function getEverybody() {
+    getData(url).then(data => {
+
+        const persons = data.data;
+        console.log(persons);
+
+        const element = document.querySelector("section");
+        while (element.firstChild) {
+            element.removeChild(element.firstChild);
+        }
+
+        const result = persons.filter((period) => word.length > 6);
+
+        persons.forEach(persons => {
+            let personName = persons.name;
+            let personImgSrc = persons.image;
+            let personTagline = persons.tagline;
+
+            personHTML =
+                `<article>
+                <img src=" https://fdnd.directus.app/assets/${personImgSrc}" alt="${personName}">
+                <h3>${personName}</h3>
+                <p>${personTagline}</p>
+            </article>`;
+            list.insertAdjacentHTML('beforeend', personHTML);
+        });
+
+    });
+}
+
+async function getData(URL) {
+    return (
+        fetch(URL)
+            .then(
+                response => response.json()
+            )
+            .then(
+                jsonData => { return jsonData }
+            )
+    );
+}
