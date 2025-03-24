ab923c0414f45ff23daac882b399b1761c6f1cec
Stephan Kok <stephan.kok.345@gmail.com>
2025-03-24 10:38:09 +0100

    link script & styling

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+async function getData() {
+    const url = "https://fdnd.directus.app/items/women_in_tech";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
