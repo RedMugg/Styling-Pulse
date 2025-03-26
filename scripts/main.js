02f7df0e24ddd8ff15a2e861ba9ecafd30be56f0
Elton Irokromo <e_irokromo@hotmail.com>
2025-03-26 14:31:04 +0100

    podium

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,13 @@
-
 const url = "https://fdnd.directus.app/items/women_in_tech";
-const list = document.querySelector('section:nth-of-type(1)>ul');
-const personDetail = document.querySelector('section:nth-of-type(2)');
+const list = document.querySelector("section:nth-of-type(1)>ul");
+const personDetail = document.querySelector("section:nth-of-type(2)");
 
-
-const selection = document.querySelector('select');
-const iFrame = document.querySelector('iframe');
+const selection = document.querySelector("select");
+const iFrame = document.querySelector("iframe");
 
 // Get all stage elements
-const gordijnenL = document.querySelector('.gordijnen');
-const gordijnenR = document.querySelector('.right')
+const gordijnenL = document.querySelector(".gordijnen");
+const gordijnenR = document.querySelector(".right");
 
 var gordijnenOpen = false;
 
@@ -17,163 +15,161 @@ var gordijnenOpen = false;
 getAllPersons();
 
 function getAllPersons() {
-    getData(url).then(data => {
+  getData(url).then((data) => {
+    const persons = data.data;
 
-        const persons = data.data;
+    var result = persons;
 
-        var result = persons;
+    result.forEach((result) => {
+      let personName = result.name;
+      let personImgSrc = result.image;
+      let personTagline = result.tagline;
+      let personID = result.id;
 
-        result.forEach(result => {
-            let personName = result.name;
-            let personImgSrc = result.image;
-            let personTagline = result.tagline;
-            let personID = result.id;
-
-            // Add info to template and repeat for everyone
-            personHTML =
-                `<li id="${personID}">
+      // Add info to template and repeat for everyone
+      personHTML = `<li id="${personID}">
                 <article>
                 <img src=" https://fdnd.directus.app/assets/${personImgSrc}" alt="${personName}">
                 <h3>${personName}</h3>
             </article>
             </li>`;
-            list.insertAdjacentHTML('beforeend', personHTML);
-        });
-        const allListItems = document.querySelectorAll('.scroll-container li')
-        // we halen alle list items op en loopen er doorheen
-        allListItems.forEach(listItem => {
-            // gebruik voortaan een button, voor nu ok
-            listItem.addEventListener('click', getID)
-        })
-
+      list.insertAdjacentHTML("beforeend", personHTML);
     });
+    const allListItems = document.querySelectorAll(".scroll-container li");
+    // we halen alle list items op en loopen er doorheen
+    allListItems.forEach((listItem) => {
+      // gebruik voortaan een button, voor nu ok
+      listItem.addEventListener("click", getID);
+    });
+  });
 }
 
-
 // Filter the selection of women and refresh who is shown
-selection.addEventListener('change', function getPersonSelection() {
-    getData(url).then(data => {
+selection.addEventListener("change", function getPersonSelection() {
+  getData(url).then((data) => {
+    const persons = data.data;
 
-        const persons = data.data;
+    var result = persons;
+    console.log(selection.value);
 
-        var result = persons;
-        console.log(selection.value);
+    switch (selection.value) {
+      case "all":
+        result = persons;
+        console.log(result);
+        break;
+      case "webWeirdness":
+        result = persons.filter((period) =>
+          period.period.includes("web weirdness")
+        );
+        console.log(result);
+        break;
+      case "webPioneer":
+        result = persons.filter((period) =>
+          period.period.includes("web pioneer")
+        );
+        console.log(result);
+        break;
+      case "webMaturity":
+        result = persons.filter((period) =>
+          period.period.includes("web maturity")
+        );
+        console.log(result);
+        break;
+      case "wayBack":
+        result = persons.filter((period) => period.period.includes("way back"));
+        console.log(result);
+        break;
+    }
 
-        switch (selection.value) {
-            case "all":
-                result = persons;
-                console.log(result);
-                break;
-            case "webWeirdness":
-                result = persons.filter((period) => (period.period.includes("web weirdness")));
-                console.log(result);
-                break;
-            case "webPioneer":
-                result = persons.filter((period) => (period.period.includes("web pioneer")));
-                console.log(result);
-                break;
-            case "webMaturity":
-                result = persons.filter((period) => (period.period.includes("web maturity")));
-                console.log(result);
-                break;
-            case "wayBack":
-                result = persons.filter((period) => (period.period.includes("way back")));
-                console.log(result);
-                break;
-        }
+    const element = document.querySelector("section>ul");
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
 
-        const element = document.querySelector("section>ul");
-        while (element.firstChild) {
-            element.removeChild(element.firstChild);
-        }
+    result.forEach((result) => {
+      let personName = result.name;
+      let personImgSrc = result.image;
+      let personTagline = result.tagline;
+      let personID = result.id;
 
-        result.forEach(result => {
-            let personName = result.name;
-            let personImgSrc = result.image;
-            let personTagline = result.tagline;
-            let personID = result.id;
-
-            // Add info to template and repeat for everyone
-            personHTML =
-                `<li id="${personID}" onclick="getID(this.id)">
+      // Add info to template and repeat for everyone
+      personHTML = `<li id="${personID}" onclick="getID(this.id)">
                 <article>
                 <img src="https://fdnd.directus.app/assets/${personImgSrc}" alt="${personName}">
                 <h3>${personName}</h3>
             </article>
             </li>`;
-            list.insertAdjacentHTML('beforeend', personHTML);
-        });
+      list.insertAdjacentHTML("beforeend", personHTML);
     });
+  });
 });
-
-
 
 // Get the clicked on persons ID
 function getID(event) {
-    const personIDClicked = event.target.id
-    if (gordijnenOpen == false) {
-        gordijnenL.style.animation = "gordijnenL 6s ease-in-out";
-        gordijnenL.style.animationFillMode = "forwards";
-        gordijnenR.style.animation = "gordijnenR 6s ease-in-out";
-        gordijnenR.style.animationFillMode = "forwards";
+  const personIDClicked = event.target.id;
+  if (gordijnenOpen == false) {
+    gordijnenL.style.animation = "gordijnenL 6s ease-in-out";
+    gordijnenL.style.animationFillMode = "forwards";
+    gordijnenR.style.animation = "gordijnenR 6s ease-in-out";
+    gordijnenR.style.animationFillMode = "forwards";
 
-        gordijnenOpen = true;
-    } else if (gordijnenOpen == true) {
+    gordijnenOpen = true;
+  } else if (gordijnenOpen == true) {
+    gordijnenL.style.animation = "none";
+    gordijnenR.style.animation = "none";
+    setTimeout(() => {
+      gordijnenL.style.animation = "gordijnenHeropenL 6s ease-in-out";
+      gordijnenL.style.animationFillMode = "forwards";
 
-        gordijnenL.style.animation = "none";
-        gordijnenR.style.animation = "none";
-        setTimeout(() => {
-            gordijnenL.style.animation = "gordijnenHeropenL 6s ease-in-out";
-            gordijnenL.style.animationFillMode = "forwards";
+      gordijnenR.style.animation = "gordijnenHeropenR 6s ease-in-out";
+      gordijnenR.style.animationFillMode = "forwards";
+    }, 10);
+  }
+  const scrollContainer = document.querySelector(".scroll-container ul");
+  console.log(event.target.offsetLeft);
+  scrollContainer.scrollTo({
+    left: event.target.offsetLeft,
+    top: 0,
+    behavior: "smooth",
+  });
 
-            gordijnenR.style.animation = "gordijnenHeropenR 6s ease-in-out";
-            gordijnenR.style.animationFillMode = "forwards";
-        }, 10);
-    }
-    const scrollContainer = document.querySelector('.scroll-container ul')
-    console.log(event.target.offsetLeft)
-    scrollContainer.scrollTo({left: event.target.offsetLeft, top: 0, behavior: 'smooth'
-    })
+  getData(url).then((data) => {
+    const persons = data.data;
+    console.log(persons);
 
-    getData(url).then(data => {
+    persons.forEach((persons) => {
+      setTimeout(() => {
+        if (persons.id == personIDClicked) {
+          let personID = persons.id;
+          let personName = persons.name;
+          let personImgSrc = persons.image;
+          let personTagline = persons.tagline;
+          let short_name = persons.short_name;
+          let period = persons.period;
+          let website = persons.website;
+          let country = persons.country;
+          let github = persons.github;
+          let work = persons.work;
 
-        const persons = data.data;
-        console.log(persons);
+          let personCodepen = persons.codepen;
+          let personPen = persons.codepen_demo;
+          let personFullPenLink = personCodepen + "/embed/" + personPen;
 
-        persons.forEach(persons => {
+          const element = document.querySelector("section:nth-of-type(2)");
+          while (element.firstChild) {
+            element.removeChild(element.firstChild);
+          }
 
-            setTimeout(() => {
-                if (persons.id == personIDClicked) {
-                    let personID = persons.id;
-                    let personName = persons.name;
-                    let personImgSrc = persons.image;
-                    let personTagline = persons.tagline;
-                    let short_name = persons.short_name;
-                    let period = persons.period;
-                    let website = persons.website;
-                    let country = persons.country;
-                    let github = persons.github;
-                    let work = persons.work;
+          // iFrame.src = personFullPenLink;
 
-                    let personCodepen = persons.codepen;
-                    let personPen = persons.codepen_demo;
-                    let personFullPenLink = personCodepen + "/embed/" + personPen;
+          personHTML =
+            // `<article id="${personID}" onclick="getID(this.id)">
+            // <img src=" https://fdnd.directus.app/assets/${personImgSrc}" alt="${personName}">
+            // <h3>${personName}</h3>
+            // <p>${personTagline}</p>
+            // </article>`;
 
-                    const element = document.querySelector("section:nth-of-type(2)");
-                    while (element.firstChild) {
-                        element.removeChild(element.firstChild);
-                    }
-
-                    // iFrame.src = personFullPenLink;
-
-                    personHTML =
-                        // `<article id="${personID}" onclick="getID(this.id)">
-                        // <img src=" https://fdnd.directus.app/assets/${personImgSrc}" alt="${personName}">
-                        // <h3>${personName}</h3>
-                        // <p>${personTagline}</p>
-                        // </article>`;
-
-                        `   <article class="personsTemplate" id="${personID}" onclick="getID(this.id)">
+            `   <article class="personsTemplate" id="${personID}" onclick="getID(this.id)">
                                 <ul>
                                     <li><img src=" https://fdnd.directus.app/assets/${personImgSrc}" alt="${personName}"></li>
                                     <li>
@@ -182,31 +178,28 @@ function getID(event) {
                                     </li>
                                 </ul>
                                 <ul>
-                                    <li><p>Short_name:</p><p>Period:</p><p>Country:</p><p>Work:</p><p>Website:</p><p>Github:</p><p>Codepen:</p></li>
-                                    <li><p>${short_name}</p><p>${period}</p><p>${country}</p><p>${work}</p><a href="${website}">${personName}</a><a href="${github}">${github}</a><a href="${personCodepen}">${personCodepen}</a></li>
+                                    <li><p>Short_name:</p><p>Period:</p><p>Country:</p><p>Work:</p></li>
+                                    <li><p>${short_name}</p><p>${period}</p><p>${country}</p><p>${work}</p></li>
+                                    <div><a href="${website}"><img src="../img/website.png" alt="website"></a><a href="${github}"><img src="../img/github.png" alt="github">
+                                    </a><a href="${personCodepen}"><img src="../img/codepen.png" alt="codepen"></a></div>
                                 </ul>
-                                <iframe src="https://www.w3schools.com" title="W3Schools Free Online Web Tutorials"></iframe>
+                                <iframe src="${personFullPenLink}"></iframe>
                             </article>`;
 
-                    personDetail.insertAdjacentHTML('beforeend', personHTML);
-                } else {
-                    console.log("Werkt niet");
-                }
-            }, 2000);
-        })
+          personDetail.insertAdjacentHTML("beforeend", personHTML);
+        } else {
+          console.log("Werkt niet");
+        }
+      }, 2000);
     });
+  });
 }
-
 
 // Basic function to retrieve data from the desired url
 async function getData(URL) {
-    return (
-        fetch(URL)
-            .then(
-                response => response.json()
-            )
-            .then(
-                jsonData => { return jsonData }
-            )
-    );
+  return fetch(URL)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      return jsonData;
+    });
 }
